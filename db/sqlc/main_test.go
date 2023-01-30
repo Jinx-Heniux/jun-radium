35f0fdb53749217adb6d73b2cd51d4952f9f0dd2
zhs2si <zhs2si@bosch.com>
2023-01-30 14:25:04 +0100

    configuration info with viper

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -6,20 +6,20 @@ import (
 	"os"
 	"testing"
 
+	"github.com/Jinx-Heniux/jun-radium-go-gin/util"
 	_ "github.com/lib/pq"
-)
-
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:secret@localhost:5433/radium?sslmode=disable"
 )
 
 var testQueries *Queries
 var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-	var err error
-	testDB, err = sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
--- main.go
+++ main.go
@@ -6,18 +6,17 @@ import (
 
 	"github.com/Jinx-Heniux/jun-radium-go-gin/api"
 	db "github.com/Jinx-Heniux/jun-radium-go-gin/db/sqlc"
+	"github.com/Jinx-Heniux/jun-radium-go-gin/util"
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver = "postgres"
-	// dbSource      = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
-	dbSource      = "postgresql://root:secret@localhost:5433/radium?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
-)
-
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
@@ -25,7 +24,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("cannot start server:", err)
 	}
